32d8519bd3c5a0e68202514b1bc75ff5a9df4775
John Meguerian <johnmeguerian@gmail.com>
2020-08-16 11:46:22 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="container">
+		<h1>Bread Math</h1>
+		<label>Target Dough Weight(g)
+		<input v-model.number="tdw" type="number" step="1" />
+		</label>
+		<label>Hydration(%)
+		<input v-model.number="hydration" type="number" step="1" />
+		</label>
+		<table>
+			<tr>
+				<th>Ingredient</th>
+				<th>Amount(g)</th>
+			</tr>
+			<tr v-for="(output, name) in outputs" :key="name">
+				<td>{{ name }}</td>
+				<td>{{ output }}</td>
+			</tr>
+		</table>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		data() {
+			return {
+				hydration: 72,
+				tdw: 1000,
+			}
+		},
+		computed: {
+			percentHydration() { return this.hydration / 100 },
+			flour() { return Math.round(this.tdw / (1 + this.percentHydration + 0.025))},
+			water() { return Math.round(this.flour * this.percentHydration) },
+			salt() { return Math.round(this.flour * 0.025)},
+			starter() { return Math.round(this.flour * 0.2)},
+			outputs() {
+				return {
+					flour: Math.round(this.flour - this.starter / 2),
+					water: Math.round(this.water - this.starter / 2),
+					salt: this.salt,
+					starter: this.starter
+				}
+			}
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+	.container {
+    max-width: 40ch;
+    margin: 0 auto;
+    padding: 5vmin;
+  }
+
+  h1 {
+    letter-spacing: -0.02em;
+  }
+
+  td {
+    text-transform: capitalize;
+  }
+
+  tr > :nth-child(2) {
+    text-align: right;
+  }
 </style>
